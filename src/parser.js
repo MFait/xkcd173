3b4e553d9bbf9b03983ea5dc555985fcd49af269
mfait <mfait@thoughtworks.com>
2016-05-02 05:21:48 +0200

    refactored parser

--- src/parser.js
+++ src/parser.js
@@ -8,34 +8,33 @@ function add(relationships, from, to, type) {
     relationships[from][to] = type;
 }
 
+function checkMatch(input, regex, handleMatch) {
+    var matches = regex.exec(input);
+
+    if (matches) {
+        handleMatch(matches);
+    }
+}
+
 var parser =  {
 
     parse: function(raw) {
         var relationships = {};
 
-        var regex = /(.*?) and (.*?) are friends/g;
-        var matches = regex.exec(raw);
-
-        if (matches) {
+        checkMatch(raw, /(.*?) and (.*?) are friends/g, function(matches) {
             add(relationships, matches[1], matches[2], relationshipTypes.FRIENDS);
             add(relationships, matches[2], matches[1], relationshipTypes.FRIENDS);
-        }
+        });
 
-        regex = /(.*?) and (.*?) are acquaintances/g;
-        matches = regex.exec(raw);
-
-        if (matches) {
+        checkMatch(raw, /(.*?) and (.*?) are acquaintances/g, function(matches) {
             add(relationships, matches[1], matches[2], relationshipTypes.ACQUAINTANCES);
             add(relationships, matches[2], matches[1], relationshipTypes.ACQUAINTANCES);
-        }
+        });
 
-        regex = /(.*?) and (.*?) are in a relationship/g;
-        matches = regex.exec(raw);
-
-        if (matches) {
+        checkMatch(raw, /(.*?) and (.*?) are in a relationship/g, function(matches) {
             add(relationships, matches[1], matches[2], relationshipTypes.IN_A_RELATIONSHIP);
             add(relationships, matches[2], matches[1], relationshipTypes.IN_A_RELATIONSHIP);
-        }
+        });
 
         return relationships;
     }
