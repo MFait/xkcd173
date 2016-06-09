6a1dbd121d8e507871f21b9a7448c51eac810da4
mfait <mfait@thoughtworks.com>
2016-06-09 22:14:48 +0200

    ...

--- movie-seating.js
+++ movie-seating.js
@@ -2,15 +2,15 @@ var parser = require('./src/parser.js');
 var seater = require('./src/seater.js');
 
 var raw =
-    'Anna and Michael are in a relationship\n' +
-    'Lena and Martin are in a relationship\n' +
-    'Anna and Lena are friends\n' +
-    'Anna and Martin are acquaintances\n' +
-    'Lena and Michael are acquaintances\n' +
-    'Folker and Sabrina are in a relationship\n' +
-    'Folker and Michael are friends\n' +
-    'Martin has a crush on Sabrina\n' +
-    'Anna and Folker are acquaintances\n';
+    'Alice and Bob are in a relationship\n' +
+    'Christine and David are in a relationship\n' +
+    'Alice and Christine are friends\n' +
+    'Alice and David are acquaintances\n' +
+    'Christine and Bob are acquaintances\n' +
+    'Ed and Gil are in a relationship\n' +
+    'Ed and Bob are friends\n' +
+    'David has a crush on Gil\n' +
+    'Alice and Ed are acquaintances\n';
 
 var relationships = parser.parse(raw);
 var sequences = seater.allSequences(relationships);
